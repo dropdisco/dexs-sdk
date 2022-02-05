30eaa8e8ca07de8f12431e2592d2f32291d85629
Agin-DropDisco <76779267+Agin-DropDisco@users.noreply.github.com>
2022-02-05 10:44:51 +0700

    Delete dist directory

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import JSBI from 'jsbi';
-export { JSBI };
-export { parseBigintIsh } from './utils';
-export * from './errors';
-export * from './entities';
-export * from './router';
-export * from './fetcher';
-export * from './constants';

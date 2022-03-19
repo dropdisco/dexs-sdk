29922ef5b61a5d44d53eb5ac66244865c1045b3f
Agin-DropDisco <76779267+Agin-DropDisco@users.noreply.github.com>
2022-03-20 02:27:32 +0700

    Add files via upload

---
+++ src/entities/token.ts
@@ -0,0 +1,135 @@
+import invariant from 'tiny-invariant'
+import { ChainId } from '../constants'
+import { validateAndParseAddress } from '../utils'
+import { Currency } from './currency'
+
+/**
+ * Represents an ERC20 token with a unique address and some metadata.
+ */
+export class Token extends Currency {
+  public readonly chainId: ChainId
+  public readonly address: string
+
+  public static readonly WETH: { [key: number]: Token } = {
+    [ChainId.MAINNET]: new Token(
+      ChainId.MAINNET,
+      '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+      18,
+      'WETH',
+      'Wrapped Ether'
+    ),
+    [ChainId.RINKEBY]: new Token(
+      ChainId.RINKEBY,
+      '0xc778417E063141139Fce010982780140Aa0cD5Ab',
+      18,
+      'WETH',
+      'Wrapped Ether'
+    ),
+    [ChainId.HARMONY]: new Token(
+      ChainId.HARMONY,
+      '0xcF664087a5bB0237a0BAd6742852ec6c8d69A27a',
+      18,
+      'WONE',
+      'Wrapped ONE'
+    ),
+    [ChainId.HARMONY_TESTNET]: new Token(
+      ChainId.HARMONY_TESTNET,
+      '0x7466d7d0C21Fa05F32F5a0Fa27e12bdC06348Ce2',
+      18,
+      'WONE',
+      'Wrapped ONE'
+    ),
+    [ChainId.MUMBAI]: new Token(
+      ChainId.MUMBAI,
+      '0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889',
+      18,
+      'WMATIC',
+      'Wrapped Matic'
+    )
+  }
+
+  // TODO:
+  public static readonly xDEXS: { [key: number]: Token } = {
+    [ChainId.RINKEBY]: new Token(ChainId.RINKEBY, '0xA9c6d7F92a894310B9C04968326A9dE6D0e38724', 18, 'DEZU', 'DexSwapZoo'),
+    [ChainId.HARMONY]: new Token(ChainId.HARMONY, '0x6d3363020963a64B4DfA8f7F9f4Dc0cBaDCe7858', 18, 'DEZU', 'DexSwapZoo'),
+    [ChainId.HARMONY_TESTNET]: new Token(ChainId.HARMONY_TESTNET, '0xbE40C5586edFAB7D296d1BF0fBd8bB56Ca4436CC', 18, 'DEZU', 'DexSwapZoo'),
+    [ChainId.MUMBAI]: new Token(ChainId.MUMBAI, '0xd91e528577e1Caf2edb60d86ae2AFaEeF405E3F6', 18, 'DEZU', 'DexSwapZoo')
+  }
+
+  public static readonly WONE: { [key: number]: Token } = {
+    [ChainId.HARMONY]: new Token(ChainId.HARMONY, '0xcF664087a5bB0237a0BAd6742852ec6c8d69A27a', 18, 'WONE', 'Wrapped One'),
+    [ChainId.HARMONY_TESTNET]: new Token(ChainId.HARMONY_TESTNET, '0x7466d7d0C21Fa05F32F5a0Fa27e12bdC06348Ce2', 18, 'WONE', 'Wrapped One')
+  }
+
+  public static readonly WMATIC: { [key: number]: Token } = {
+    // [ChainId.MATIC]: new Token(ChainId.MATIC, '', 18, ''WMATIC', 'Wrapped Matic'),
+    [ChainId.MUMBAI]: new Token(ChainId.MUMBAI, '0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889', 18, 'WMATIC', 'Wrapped Matic')
+  }
+
+  private static readonly NATIVE_CURRENCY_WRAPPER: { [chainId in ChainId]: Token } = {
+    [ChainId.MAINNET]: Token.WETH[ChainId.MAINNET],
+    [ChainId.RINKEBY]: Token.WETH[ChainId.RINKEBY],
+    [ChainId.HARMONY]: Token.WONE[ChainId.HARMONY],
+    [ChainId.HARMONY_TESTNET]: Token.WETH[ChainId.HARMONY_TESTNET],
+    [ChainId.MUMBAI]: Token.WMATIC[ChainId.MUMBAI],
+  }
+
+  public constructor(chainId: ChainId, address: string, decimals: number, symbol?: string, name?: string) {
+    super(decimals, symbol, name)
+    this.chainId = chainId
+    this.address = validateAndParseAddress(address)
+  }
+
+  /**
+   * Returns true if the two tokens are equivalent, i.e. have the same chainId and address.
+   * @param other other token to compare
+   */
+  public equals(other: Token): boolean {
+    // short circuit on reference equality
+    if (this === other) {
+      return true
+    }
+    return this.chainId === other.chainId && this.address === other.address
+  }
+
+  /**
+   * Returns true if the address of this token sorts before the address of the other token
+   * @param other other token to compare
+   * @throws if the tokens have the same address
+   * @throws if the tokens are on different chains
+   */
+  public sortsBefore(other: Token): boolean {
+    invariant(this.chainId === other.chainId, 'CHAIN_IDS')
+    invariant(this.address !== other.address, 'ADDRESSES')
+    return this.address.toLowerCase() < other.address.toLowerCase()
+  }
+
+  public static getNativeWrapper(chainId: ChainId): Token {
+    return Token.NATIVE_CURRENCY_WRAPPER[chainId]
+  }
+
+  public static isNativeWrapper(token: Token): boolean {
+    return Token.NATIVE_CURRENCY_WRAPPER[token.chainId].equals(token)
+  }
+}
+
+/**
+ * Compares two currencies for equality
+ */
+export function currencyEquals(currencyA: Currency, currencyB: Currency): boolean {
+  if (currencyA instanceof Token && currencyB instanceof Token) {
+    return currencyA.equals(currencyB)
+  } else if (currencyA instanceof Token) {
+    return false
+  } else if (currencyB instanceof Token) {
+    return false
+  } else {
+    return currencyA === currencyB
+  }
+}
+
+// reexport for convenience
+export const WETH = Token.WETH
+export const xDEXS = Token.xDEXS
+export const WONE = Token.WONE
+export const WMATIC = Token.WMATIC
